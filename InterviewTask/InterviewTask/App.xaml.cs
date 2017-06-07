00e2db38d21004660ce3d51eb6d8cf860925dfdd
Steven Nguyen <stevennhnk113@gmail.com>
2017-06-06 19:05:10 -0700

    add support for android and Ios

---
+++ InterviewTask/InterviewTask/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using InterviewTask.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace InterviewTask
+{
+	public partial class App : Application
+	{
+        public App()
+		{
+			InitializeComponent();
+
+			SetMainPage();
+		}
+
+		public static void SetMainPage()
+		{
+			LoginPage loginPage = new LoginPage();
+			Current.MainPage = new NavigationPage(loginPage);
+			NavigationPage.SetHasNavigationBar(Current.MainPage.Navigation.NavigationStack[Current.MainPage.Navigation.NavigationStack.Count - 1], false);
+			MessagingCenter.Subscribe<ContentPage>(Current.MainPage, "RemoveNavigationBar", (sender) => {
+				NavigationPage.SetHasNavigationBar(Current.MainPage.Navigation.NavigationStack[Current.MainPage.Navigation.NavigationStack.Count - 1], false);
+			});
+		}
+	}
+}
---
+++ InterviewTask/InterviewTask/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace InterviewTask.ViewModels
+{
+	public class LoginViewModel : BaseViewModel
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+
+		public LoginViewModel()
+		{
+			Username = string.Empty;
+			Password = string.Empty;
+		}
+	}
+}
